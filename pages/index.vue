bbe9f474681a2924bdc44df364af3782c49d91a5
TigRig <tigrig.crex@gmail.com>
2020-03-16 19:41:48 +0900

    Refactor: separate the card editor modules

--- pages/index.vue
+++ pages/index.vue
@@ -47,81 +47,10 @@
         </b-card-text>
       </b-card>
     </b-card-group>
-    <b-button class="mt-4" @click="showCardEditor('')">
+    <b-button class="mt-4" @click="showCardEditor()">
       グループを追加
     </b-button>
-    <b-modal id="modal-card-editor" size="md" centered :title="cardEditorTitle">
-      <b-form>
-        <label for="inline-form-input-name">タイトル</label>
-        <b-input
-          id="inline-form-input-name"
-          v-model="edittingCardValue.title"
-          :state="cardValidState.title"
-          placeholder="タイトルを入力してください"
-        ></b-input>
-      </b-form>
-      <b-form class="mt-4">
-        <label>リセット期間</label>
-        <b-form inline>
-          <label class="sr-only" for="inline-form-input-name">値</label>
-          <b-input-group class="w-25 mb-2 mr-sm-2 mb-sm-0">
-            <b-input
-              id="inline-form-input-name"
-              v-model="edittingCardValue.denominator"
-              :state="cardValidState.denominator"
-              type="number"
-            ></b-input>
-          </b-input-group>
-
-          <label class="sr-only" for="inline-form-custom-select-pref">
-            単位
-          </label>
-          <b-form-select
-            id="inline-form-custom-select-pref"
-            v-model="edittingCardValue.denominatorUnit"
-            :state="cardValidState.denominatorUnit"
-            class="w-25 mb-2 mr-sm-2 mb-sm-0"
-            :options="[
-              { text: '選択...', value: null },
-              { text: '日', value: 'day' },
-              { text: '週間', value: 'week' },
-              { text: '月', value: 'month' },
-              { text: '年', value: 'year' }
-            ]"
-          ></b-form-select>
-          <label>ごとにリセット</label>
-        </b-form>
-        <b-form class="mt-4">
-          <label>最終リセット日付</label>
-          <b-form-datepicker
-            id="input-card-last-reset-at"
-            v-model="edittingCardValue.lastResetAt"
-            class="mb-2 mr-sm-2 mb-sm-0"
-            :state="cardValidState.lastResetAt"
-          ></b-form-datepicker>
-        </b-form>
-        <b-button
-          v-show="!isEditingNewCard"
-          class="mt-4"
-          variant="danger"
-          @click="deleteCard"
-        >
-          削除
-        </b-button>
-      </b-form>
-      <template v-slot:modal-footer>
-        <b-button variant="secondary" @click="closeCardEditor">
-          キャンセル
-        </b-button>
-        <b-button
-          variant="primary"
-          :disabled="!cardValidStateAll"
-          @click="saveCard"
-        >
-          保存
-        </b-button>
-      </template>
-    </b-modal>
+    <card-editor />
   </div>
 </template>
 
@@ -133,18 +62,20 @@ import {
   BIconTrashFill,
   BIconGear
 } from 'bootstrap-vue'
-import { Card } from '../types/firestore'
+import { Card } from '~/types/firestore'
 import { firebase } from '~/plugins/firebase'
-import { cardStore, taskStore, userStore } from '~/store'
+import { cardStore, taskStore, userStore, cardEditor } from '~/store'
 import firestoreWriter from '~/assets/libs/firestoreWriter'
 import date from '~/assets/libs/date'
+import CardEditor from '~/components/card/CardEditor.vue'
 
 @Component({
   components: {
     BIconCircle,
     BIconCheckCircle,
     BIconTrashFill,
-    BIconGear
+    BIconGear,
+    CardEditor
   },
   middleware: 'fetchFirestore'
 })
@@ -240,130 +171,9 @@ export default class Index extends Vue {
   // card の更新処理
   // =================================================
 
-  get cardEditorTitle() {
-    return this.isEditingNewCard ? '新規グループ作成' : 'グループ編集'
-  }
-
-  get cardValidState() {
-    return {
-      title: this.edittingCardValue.title.length > 0,
-      denominator: this.edittingCardValue.denominator > 0,
-      denominatorUnit: this.edittingCardValue.denominatorUnit !== null,
-      lastResetAt: this.edittingCardValue.lastResetAt !== null
-    }
-  }
-
-  get cardValidStateAll() {
-    return (
-      this.cardValidState.title &&
-      this.cardValidState.denominator &&
-      this.cardValidState.denominatorUnit &&
-      this.cardValidState.lastResetAt
-    )
-  }
-
-  get isEditingNewCard() {
-    return this.edittingCardId === ''
-  }
-
-  showCardEditor(cardId: string) {
+  showCardEditor(cardId?: string) {
+    cardEditor.init(cardId)
     this.$bvModal.show('modal-card-editor')
-    this.edittingCardId = cardId
-    if (!this.isEditingNewCard) {
-      this.edittingCardValue.title = cardStore.cardList[cardId].title
-      this.edittingCardValue.denominator =
-        cardStore.cardList[cardId].denominator
-      this.edittingCardValue.denominatorUnit =
-        cardStore.cardList[cardId].denominatorUnit
-      this.edittingCardValue.lastResetAt = date.pickUpDate(
-        cardStore.cardList[cardId].lastResetAt,
-        '-'
-      )
-    }
-  }
-
-  closeCardEditor() {
-    this.$bvModal.hide('modal-card-editor')
-    this.edittingCardId = ''
-    // edittingCardValue を即時リセットすると、
-    // モーダルのフェードアウト中に値が変化してしまうので遅延
-    setTimeout(() => {
-      this.edittingCardValue = {
-        title: '',
-        position: 0,
-        denominator: 1,
-        denominatorUnit: 'day',
-        lastResetAt: date.pickUpDate(new Date(), '-')
-      }
-    }, 200)
-  }
-
-  async saveCard() {
-    // input type="number" の入力値が string っぽいのでキャスト
-    if (typeof this.edittingCardValue.denominator === 'string') {
-      this.edittingCardValue.denominator = parseInt(
-        this.edittingCardValue.denominator
-      )
-    }
-
-    if (this.isEditingNewCard) await this.addCard()
-    else await this.updateCard()
-
-    this.closeCardEditor()
-  }
-
-  async addCard() {
-    const [
-      year,
-      month,
-      day
-    ]: string[] = this.edittingCardValue.lastResetAt.split('-')
-
-    const newCard: Card = {
-      ...this.edittingCardValue,
-      position: cardStore.currentMaxPosition + 1,
-      lastResetAt: new Date(parseInt(year), parseInt(month) - 1, parseInt(day))
-    }
-
-    const { cardId, card } = await firestoreWriter.addCard(
-      userStore.id || '',
-      newCard.title,
-      newCard.position,
-      newCard.denominator,
-      newCard.denominatorUnit,
-      newCard.lastResetAt
-    )
-    cardStore.addCard({ cardId, card })
-  }
-
-  async updateCard() {
-    const [
-      year,
-      month,
-      day
-    ]: string[] = this.edittingCardValue.lastResetAt.split('-')
-
-    const cardId: string = this.edittingCardId
-    const card: Card = {
-      ...this.edittingCardValue,
-      position: cardStore.cardList[cardId].position,
-      lastResetAt: new Date(parseInt(year), parseInt(month) - 1, parseInt(day))
-    }
-
-    cardStore.updateCard({ cardId, card })
-    await firestoreWriter.updateCard(userStore.id || '', cardId)
-  }
-
-  async deleteCard() {
-    const result: boolean = await this.$bvModal.msgBoxConfirm(
-      '削除してもよろしいですか？'
-    )
-
-    if (result) {
-      await firestoreWriter.deleteCard(userStore.id || '', this.edittingCardId)
-      cardStore.deleteCard({ cardId: this.edittingCardId })
-      this.closeCardEditor()
-    }
   }
 
   // =================================================
--- utils/store-accessor.ts
+++ utils/store-accessor.ts
@@ -4,19 +4,29 @@ import { Store } from 'vuex'
 import { getModule } from 'vuex-module-decorators'
 import User from '~/store/user'
 import CardStore from '~/store/cardStore'
+import CardEditor from '~/store/cardEditor'
 import TaskStore from '~/store/taskStore'
 import Others from '~/store/others'
 
 let userStore: User
 let cardStore: CardStore
+let cardEditor: CardEditor
 let taskStore: TaskStore
 let othersStore: Others
 
 function initializeStores(store: Store<any>): void {
   userStore = getModule(User, store)
   cardStore = getModule(CardStore, store)
+  cardEditor = getModule(CardEditor, store)
   taskStore = getModule(TaskStore, store)
   othersStore = getModule(Others, store)
 }
 
-export { initializeStores, userStore, cardStore, taskStore, othersStore }
+export {
+  initializeStores,
+  userStore,
+  cardStore,
+  cardEditor,
+  taskStore,
+  othersStore
+}
